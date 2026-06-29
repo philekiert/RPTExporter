296ed0ca1266769f5dc160e5cdb3df976df6f54b
Phil Ekiert <philekiert@live.co.uk>
2026-06-29 11:41:27 +0100

    Icons now included properly

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using BridgeOpsClient.DialogWindows;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
-
-using BridgeOpsClient.DialogWindows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace RPTExporter
 {
@@ -17,5 +18,31 @@ namespace RPTExporter
             dialog.Owner = w;
             dialog.ShowDialog();
         }
+
+        private void DateTimeClearValueHandler(object o, EventArgs e)
+        {
+            try
+            {
+                if (o is TextBox txt && txt.Text == "")
+                {
+                    DatePicker dp = GetParentControl<DatePicker>(txt);
+                    if (dp != null)
+                        dp.SelectedDate = null;
+                }
+            }
+            catch { }
+        }
+        public static T GetParentControl<T>(DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+            while (parentObject != null && !(parentObject is Window))
+            {
+                parentObject = VisualTreeHelper.GetParent(parentObject);
+                if (parentObject is T t)
+                    return t;
+            }
+            return null;
+        }
+
     }
 }
